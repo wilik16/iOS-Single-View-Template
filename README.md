beff7cb280daea25f9691d94c0ff3ec3c66cf69f
wilik <wilik@bukalapak.com>
2023-09-12 21:11:02 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # iOS-Single-View-Template
+
---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Wilik on 12/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
